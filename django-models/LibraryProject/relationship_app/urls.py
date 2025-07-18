9c3b3c0de6186a8de14b1456dca5b3123b99beaf
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-18 14:45:19 +0200

    added models fo the library relationship app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,4 +19,8 @@ urlpatterns = [
     path('admin/',views. admin_view, name='admin_dashboard'),
     path('librarian/', views.librarian_view, name='librarian_dashboard'),
     path('member/', views.member_view, name='member_dashboard'),
+
+    path("add_book/", views.add_book, name='add_book'),
+    path("edit_book/", views.edit_book, name='edit_book'),
+    path("delete_book/", views.delete_book, name='delete_book'),
 ]

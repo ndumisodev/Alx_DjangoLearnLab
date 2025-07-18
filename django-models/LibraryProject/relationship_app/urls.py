912b6a25e70fb05c17daea908405f3b42a95b5bc
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-18 14:11:00 +0200

    added models fo the library relationship app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,6 +7,8 @@ from .views import (
 )
 from . import views
 
+from . import admin_view
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -16,4 +18,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), redirect_authenticated_user=True, name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), redirect_authenticated_user=True, name='logout'),
     path('register/', views.register, name='register'),
+
+    # Function-based views
+    path('admin/', admin_view.Admin, name='admin-view'),
+    # path('librarian/', librarian_view, name='librarian-view'),
+    # path('member/', member_view, name='member-view'),
 ]

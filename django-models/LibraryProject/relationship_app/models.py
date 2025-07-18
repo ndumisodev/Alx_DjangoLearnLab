11136f9391d0fa2302f615272cb318c920aaf066
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-18 13:53:24 +0200

    added models fo the library relationship app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class UserProfile(models.Model) :
+class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('ADMIN','Admin'),
         ('LIBRARIAN', 'Librarian'),

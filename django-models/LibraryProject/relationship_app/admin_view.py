8ffea75cae88c0f368e2a09c50c70d804552b7db
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-18 14:06:18 +0200

    added models fo the library relationship app

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic import View
 
-def Admin(user):
+def check_admin(user):
     return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'ADMIN'
 
 def check_librarian(user):
@@ -13,8 +13,8 @@ def check_member(user):
     return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'MEMBER'
 
 @login_required
-@user_passes_test(Admin)
-def admin_view(request):
+@user_passes_test(check_admin)
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @login_required
@@ -30,7 +30,7 @@ def member_view(request):
 # Alternative class-based views
 class AdminView(UserPassesTestMixin, View):
     def test_func(self):
-        return Admin(self.request.user)
+        return check_admin(self.request.user)
     
     def get(self, request):
         return render(request, 'relationship_app/admin_view.html')

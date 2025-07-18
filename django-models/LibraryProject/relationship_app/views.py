b7d2025adca5f66bdfa5962e26c5c6e05b76d479
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-18 12:29:39 +0200

    added models fo the library relationship app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 def list_books(request) :
     books = Book.objects.all()
@@ -29,7 +30,12 @@ class CustomLogoutView(LogoutView) :
 class RegisterView(CreateView) :
     form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('list_books')
+
+    def form_valid(self, form) :
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
 
 
 

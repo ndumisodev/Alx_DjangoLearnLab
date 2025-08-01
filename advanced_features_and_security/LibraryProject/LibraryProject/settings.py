0f42e11deb6452fef388d746a625e8ccd477351b
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-01 21:35:24 +0200

    adding permisions in book model

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -160,3 +160,21 @@ X_FRAME_OPTIONS = 'DENY'
 
 # Avoid content-type sniffing
 SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+# Enable browser’s XSS protection
+SECURE_BROWSER_XSS_FILTER = True
+
+
+# Enforce HTTPS redirect
+SECURE_SSL_REDIRECT = True  # Redirect all HTTP to HTTPS
+
+# Enable HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
+# Ensure cookies are only sent over HTTPS
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True

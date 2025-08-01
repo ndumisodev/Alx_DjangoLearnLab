939a46c5fc2697c522ed2ee93deacc6bf71f69d2
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-01 21:10:34 +0200

    adding permisions in book model

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from django.views.decorators.csrf import csrf_protect
 
+
+@csrf_protect 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()

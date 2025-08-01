f459b3f9e9c18731cfab1f0b43e6c4fff8ffd743
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-01 20:13:18 +0200

    adding permisions in book model

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/view_books.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # your logic for creating a book
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    # your logic for editing a book
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    # your logic for deleting a book
+    pass

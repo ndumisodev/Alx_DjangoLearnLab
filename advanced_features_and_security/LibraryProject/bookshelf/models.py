537306cda7e25e05157e5530755ce54948ebe24a
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-01 20:06:06 +0200

    adding permisions in book model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -10,6 +10,13 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
     
+    class Meta:
+        permissions = [
+            ("can_view", "Can view books"),
+            ("can_create", "Can create books"),
+            ("can_edit", "Can edit books"),
+            ("can_delete", "Can delete books"),
+        ]
 
 
 class CustomUserManager(BaseUserManager):

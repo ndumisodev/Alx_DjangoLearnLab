01de921a686ae783eaa86c33f4e9c3ce218cdcd9
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-16 18:33:36 +0200

    added two serializer.py and two serializer for book andauthor

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Author, Book
+from .serializers import BookSerializer
+from rest_framework import generics, permissions
 
-# Create your views here.
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    A view to list all books or create a new book.
+    - GET request: Returns a list of all books.
+    - POST request: Creates a new book.
+    - Permissions:
+      - Any user can list books.
+      - Only authenticated users can create a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    A view to retrieve, update, or delete a single book instance.
+    - GET request: Retrieves a book by its primary key (pk).
+    - PUT/PATCH request: Updates a book.
+    - DELETE request: Deletes a book.
+    - Permissions:
+      - Any user can retrieve a single book.
+      - Only authenticated users can update or delete a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]

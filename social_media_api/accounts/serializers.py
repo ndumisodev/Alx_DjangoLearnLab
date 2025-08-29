0c3997796edbf03b7ddd8fa89c7d8a7a5da3e372
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-29 22:15:10 +0200

    user model

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,26 +6,20 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
-        # Create a new user with the create_user method
+        # Use get_user_model().objects.create_user() to create a new user
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture')
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
         )
-        # Create a token for the new user
-        Token.objects.create(user=user)
         return user
-
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(style={'input_type': 'password'})

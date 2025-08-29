bbc629c78a0076e19910743602447352a623c889
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-29 22:16:24 +0200

    user model

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,13 +13,16 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
-        # Use get_user_model().objects.create_user() to create a new user
+        # Create the user using the safe create_user method
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password']
         )
+        # Create a token for the new user and return it
+        Token.objects.create(user=user)
         return user
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(style={'input_type': 'password'})

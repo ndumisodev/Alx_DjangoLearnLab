a2a83338d437427b834202999e590a13e13a1932
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-30 14:53:36 +0200

    adding chat room and Message model classes view classes for apis

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,10 +2,10 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
-from .models import Post, Comment
-
 
 User = get_user_model()
+
+
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -34,35 +34,3 @@ class UserProfileSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(style={'input_type': 'password'})
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author = UserProfileSerializer(read_only=True)
-    
-    class Meta:
-        model = Comment
-        fields = ('id', 'author', 'content', 'created_at', 'updated_at')
-        read_only_fields = ('id', 'author', 'created_at', 'updated_at')
-
-class PostSerializer(serializers.ModelSerializer):
-    author = UserProfileSerializer(read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
-    comments_count = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = Post
-        fields = ('id', 'author', 'title', 'content', 'created_at', 'updated_at', 'comments', 'comments_count')
-        read_only_fields = ('id', 'author', 'created_at', 'updated_at')
-    
-    def get_comments_count(self, obj):
-        return obj.comments.count()
-
-class PostCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('title', 'content')
-
-class CommentCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ('content',)

d68bf8dbdb61869fcd72940eabc46e9a9f4a733d
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-30 15:14:28 +0200

    adding chat room and Message model classes view classes for apis

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,8 @@
 # accounts/urls.py
-from django.urls import path, include
+from django.urls import path
 from .views import RegistrationView, LoginView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path("api/", include("posts.urls")),
 ]

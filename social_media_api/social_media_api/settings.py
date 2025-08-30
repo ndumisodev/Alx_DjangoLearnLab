5ba7999ad3f1bc1a4b06a602e7336aa7f183af83
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-30 14:43:00 +0200

    adding chat room and Message model classes view classes for apis

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -125,3 +125,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'accounts.User' 
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.SearchFilter'],
+}

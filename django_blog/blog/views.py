a4a6bb2a9f6d0fca3dc0d4f7abecdab39b4146b1
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-30 16:39:40 +0200

    user model

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('blog-home') # Redirect to your homepage
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'form': form})

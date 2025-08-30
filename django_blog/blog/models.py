9d9a467b1e12c7a5d1464dd7521dc0c237561b96
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-30 16:59:23 +0200

    user model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,27 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
-    
-
-
-# class Post(models.Model):
-#     title = models.CharField(max_length=200)
-#     content = models.TextField()
-#     author = models.ForeignKey(User, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     tags = TaggableManager()
-
-#     def __str__(self):
-#         return self.title
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)

bb0d560455afe92fbf92858d209bb665bd3fb41e
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-30 14:57:33 +0200

    adding chat room and Message model classes view classes for apis

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,31 +5,25 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
-
-
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'bio')
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        # Use get_user_model().objects.create_user() to safely create a new user
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            password=validated_data['password']
         )
+        # Create a token for the new user and return it
         Token.objects.create(user=user)
         return user
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture')
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
